60ce406ec9eecaba30845d9e9e001f4796277fa4
Jesse Loftus <jesse.loftus@gmail.com>
2019-05-01 15:34:33 -0700

    day 3 arrays

---
+++ 10_days_javascript/3_array/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+  inputString = inputString
+    .trim()
+    .split('\n')
+    .map(string => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/**
+ *   Return the second largest number in the array.
+ *   @param {Number[]} nums - An array of numbers.
+ *   @return {Number} The second largest number in the array.
+ **/
+getSecondLargest([2, 3, 3, 6, 5]);
+function getSecondLargest(nums) {
+  // Complete the function
+  let remBig = nums.map(x => (x != Math.max(...nums) ? x : null));
+  return Math.max(...remBig);
+}
+function main() {
+  const n = +readLine();
+  const nums = readLine()
+    .split(' ')
+    .map(Number);
+
+  console.log(getSecondLargest(nums));
+}
